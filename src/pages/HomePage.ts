5b857f92d847d313c3fdf6be75ca527968a225ac
qaisarriaz24 <qaisarriaz24@gmail.com>
2026-09-02 02:31:17 +0500

    Improve Handla menu synchronization

--- src/pages/HomePage.ts
+++ src/pages/HomePage.ts
@@ -7,21 +7,23 @@ export class HomePage {
     }
 
     async navigateToTelenor() {
-    await this.page.goto("https://telenor.se/", {
-        waitUntil: "domcontentloaded",
-        timeout: 60000
-    });
-}
+        await this.page.goto("https://telenor.se/", {
+            waitUntil: "domcontentloaded",
+            timeout: 60000
+        });
+    }
 
 
-    async clickOnHandlaBredbandLink(): Promise<void>{
+    async clickOnHandlaBredbandLink(): Promise<void> {
         // handling cookies popup bfore click
         const cookies = new Cookies(this.page)
         await cookies.acceptCookiesIfPresent()
 
 
         const bredBandDropDownLink = this.page.locator('[data-test="Bredband"]').first()
-        await expect(bredBandDropDownLink).toBeVisible()
+        await expect(bredBandDropDownLink).toBeVisible({
+            timeout: 15000
+        })
         await bredBandDropDownLink.click()
 
         const clickOnBroadBandViaFiber = this.page.locator('a[href="/handla/bredband/"]').nth(1)
